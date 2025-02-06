01e2e9376d6072a8b31b94ab044c7c6f37fae0cc
Sina Arshad <sina.arshad@hotmail.com>
2025-02-06 15:00:19 +0330

    Add docstrings

--- webots_smarthome/robot.py
+++ webots_smarthome/robot.py
@@ -7,12 +7,33 @@ from .devices import Motor, DistanceSensor, Receiver, Emitter, InertialUnit, GPS
 
 @dataclass
 class RobotWheels:
+    """A class to represent the wheels of a robot.
+
+    Attributes:
+        right (Motor): The motor controlling the right wheel of the robot.
+        left (Motor): The motor controlling the left wheel of the robot.
+    """
+
     right: Motor
     left: Motor
 
 
 @dataclass
 class RobotSensors:
+    """
+    RobotSensors class represents the distance sensors attached to a robot.
+
+    Attributes:
+        front_left (DistanceSensor): Sensor located at the front left of the robot.
+        front_right (DistanceSensor): Sensor located at the front right of the robot.
+        right_front (DistanceSensor): Sensor located at the right front of the robot.
+        right_back (DistanceSensor): Sensor located at the right back of the robot.
+        back_left (DistanceSensor): Sensor located at the back left of the robot.
+        back_right (DistanceSensor): Sensor located at the back right of the robot.
+        left_front (DistanceSensor): Sensor located at the left front of the robot.
+        left_back (DistanceSensor): Sensor located at the left back of the robot.
+    """
+    
     front_left: DistanceSensor
     front_right: DistanceSensor
     right_front: DistanceSensor
@@ -25,6 +46,23 @@ class RobotSensors:
 
 class Robot(BaseRobot):
     def __init__(self, name: str, max_speed: float):
+        """Initializes the robot with the given name and maximum speed.
+
+        Args:
+            name (str): The name of the robot.
+            max_speed (float): The maximum speed of the robot.
+        Attributes:
+            max_speed (float): The maximum speed of the robot.
+            time_step (int): The basic time step of the robot.
+            emitter (Emitter): The emitter device for sending messages.
+            receiver (Receiver): The receiver device for receiving messages.
+            wheels (RobotWheels): The wheels of the robot.
+            sensors (RobotSensors): The distance sensors of the robot.
+            inertial_unit (InertialUnit, optional): The inertial unit of the robot if available.
+            gps (GPS, optional): The GPS device of the robot if available.
+            battery (float): The battery level of the robot.
+        """
+        
         super().__init__()
         self.max_speed = max_speed
         self.time_step = int(self.getBasicTimeStep())
@@ -67,5 +105,12 @@ class Robot(BaseRobot):
 
 
     def move(self, left: float, right: float):
+        """Sets the velocity of the robot's wheels.
+
+        Parameters:
+            left (float): The velocity for the left wheel.
+            right (float): The velocity for the right wheel.
+        """
+        
         self.wheels.left.velocity = left
         self.wheels.right.velocity = right
